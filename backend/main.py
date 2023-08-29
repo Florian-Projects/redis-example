e8a52e7a9af0d5e10da89cafb137c7b8b135bba9
florian <unknown>
2023-08-29 10:28:03 +0200

    fix search endpoint and enable chaching

--- backend/main.py
+++ backend/main.py
@@ -3,9 +3,10 @@ import redis
 from fastapi import FastAPI, Response
 from fastapi.middleware.cors import CORSMiddleware
 from tortoise.contrib.fastapi import register_tortoise
+from tortoise.queryset import QuerySet
 from models import Books, Books_Pydantic, SearchResponse
 
-r = redis.Redis(host='localhost', port=6379, db=0)
+r = redis.asyncio.Redis(host='localhost', port=6379, db=0)
 app = FastAPI()
 
 app.add_middleware(
@@ -16,44 +17,49 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
+async def paginate(queryset: QuerySet, limit: int, offset: int) -> QuerySet:
+    return queryset.order_by("id").offset(offset).limit(limit)
+
+
 @app.get("/status")
 async def get_status():
     return {"ok": "ok"}
 
 
-@app.get("/books/all", response_model=list[Books_Pydantic])
-async def get_all_books():
-    books = await Books_Pydantic.from_queryset(Books.all())
-    return books
-
-
-@app.get("/books/", response_model=Books_Pydantic)
+@app.get("/book/", response_model=Books_Pydantic)
 async def get_specific_book(book_id: int, response: Response):
     key = f"book:{book_id}"
     details = await r.get(key)
     if not details:
         if book := await Books.filter(id=book_id).first():
             book_pydantic = await Books_Pydantic.from_tortoise_orm(book)
-            await r.set(key, book_pydantic.model_dump_json())
+            await r.set(key, book_pydantic.model_dump_json(), ex=40)
             return book_pydantic
         else:
             response.status_code = 404
             return {}
+    else:
+        await r.expire(key, 40)
     details = json.loads(details)
     return Books_Pydantic(**details)
 
 
-@app.get("/books/search", response_model=list[SearchResponse])
-async def search_book_by_title(q: str):
-    key = f"book_search:{q}"
+@app.get("/book/search", response_model=list[SearchResponse])
+async def search_book_by_title(q: str, page_number: int = 0):
+    q = q.lower()
+    key = f"book_search:{q}:{page_number}"
     search_result = await r.get(key)
     if not search_result:
-        books = await Books_Pydantic.from_queryset(Books.filter(title__istartswith=q))
-        search_result = [SearchResponse(**book.dict()) for book in books]
-        await r.set(key, json.dumps(search_result))
-        return search_result
+        books = await Books_Pydantic.from_queryset(
+            await paginate(Books.filter(title__istartswith=q), 50, 50 * page_number))
+        search_result = json.dumps([SearchResponse(**book.dict()).model_dump_json() for book in books])
+        await r.set(key, search_result, ex=40)
+
     else:
-        return json.loads(search_result)
+        await r.expire(key, 40)
+
+    return [SearchResponse(**json.loads(rsp)) for rsp in json.loads(search_result)]
 
 
 register_tortoise(
