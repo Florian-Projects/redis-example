87fafe4c9b6f594b6da539d9b310d45d5d845bd1
Tristan Zaton <zaton@hornetsecurity.com>
2023-08-29 09:31:34 +0200

    Add frontend

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subject, takeUntil} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {BookService} from "./book.service";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit, OnDestroy {
+  private readonly destroy$ = new Subject<void>();
+
+  constructor(private readonly bookService: BookService) {}
+
+  ngOnInit() {
+    this.bookService.list().pipe(takeUntil(this.destroy$)).subscribe((books) => console.log({books}));
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
---
+++ frontend/src/app/book.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {map, Observable, pipe, tap} from "rxjs";
+
+export interface Book {
+  id: number,
+  title: string,
+  isbn: string,
+  author: string,
+  cover_picture: string,
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+  private static readonly API = "http://localhost:8000"
+
+  constructor(private readonly http: HttpClient) {}
+
+  list() {
+    return this.http.get<Array<Book>>(BookService.API + "/books/all").pipe(this.alert_on_error("Failed to fetch book list"));
+  }
+
+  get_single(id: number) {
+    return this.http.get<Book>(BookService.API + `/books?book_id=${id}`).pipe(this.alert_on_error("Failed to fetch book"));
+  }
+
+  private alert_on_error(message: string) {
+    return pipe(
+      tap({error: (e) => alert(`${message}: ${JSON.stringify(e)}`)}),
+    );
+  }
+}
