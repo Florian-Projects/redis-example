bdcf2d4cecbed742720f0c4a2a3fe16ddce67cf0
Tristan Zaton <zaton@hornetsecurity.com>
2023-09-07 15:24:02 +0200

    Add periodic update check

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,18 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { BehaviorSubject, map, startWith, Subject, switchMap } from 'rxjs';
+import {Component, isDevMode, OnDestroy, OnInit} from '@angular/core';
+import {
+  BehaviorSubject,
+  exhaustMap, filter,
+  map,
+  startWith,
+  Subject,
+  switchMap,
+  takeUntil,
+  timer
+} from 'rxjs';
 import { Book, BookService } from './book.service';
 import {environment} from "../environments/environment";
+import {SwUpdate} from "@angular/service-worker";
+import {fromPromise} from "rxjs/internal/observable/innerFrom";
 
 @Component({
   selector: 'app-root',
@@ -23,12 +34,34 @@ export class AppComponent implements OnInit, OnDestroy {
     map((response) => response?.items),
   );
 
-  constructor(private readonly bookService: BookService) {}
+  constructor(private readonly bookService: BookService, private readonly swUpdate: SwUpdate) {}
 
   ngOnInit() {
     this.websocket = new WebSocket(`${environment.wsApiBase}/book/purchases`);
     this.websocket.onmessage = (message) =>
       (this.purchases = [JSON.parse(message.data), ...this.purchases]);
+
+    if (isDevMode())
+      return;
+
+    timer(0, 5 * 60_000)
+      .pipe(
+        exhaustMap(() => fromPromise(this.swUpdate.checkForUpdate())),
+        takeUntil(this.destroy$),
+      )
+      .subscribe();
+
+    this.swUpdate.versionUpdates
+      .pipe(
+        filter((e) => e.type === 'VERSION_READY'),
+        takeUntil(this.destroy$),
+      )
+      .subscribe(
+        () =>
+          confirm(
+            'A new version of this app is available. Do you want to reload?',
+          ) && location.reload(),
+      );
   }
 
   ngOnDestroy() {
